ad1f2edfa9fffea403d52a9e3abfeccfe9e005b8
AlifJamain <67289330+AlifJamain@users.noreply.github.com>
2021-02-15 21:03:23 +0800

    Update mains_controller.php

--- controllers/mains_controller.php
+++ controllers/mains_controller.php
@@ -5,9 +5,35 @@ class MainsController
 
     public function index()
     {
-
+        session_start();
         require_once('views/main/index.php');
-        
+        require_once('models/main.php');
+        if (isset($_POST['signin'])) {
+            $username = $_POST['username'];
+            $password = md5($_POST['password']);
+            $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+            $query = DB::InstanceDB()->prepare($sql);
+            $query->execute();
+            $results = $query->fetchAll(PDO::FETCH_OBJ);
+            if ($query->rowCount() > 0) {
+                foreach ($results as $result) {
+                    $_SESSION['login'] = $_POST['username'];
+                    echo "<script type='text/javascript'> document.location = '?controller=homes&action=index'; </script>";
+                }
+            } else {
+                echo "<script>alert('Information Not in database');</script>";
+                echo "<script type='text/javascript'> document.location = '?'; </script>";
+            }
+        }
     }
 
+    public function register()
+    {
+
+        require_once('views/main/register.php');
+        require_once('models/main.php');
+        if (isset($_POST['register'])) {
+            Main::register();
+        }
+    }
 }
